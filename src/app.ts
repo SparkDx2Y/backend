7fe517e0201d7d6c3b34e641f2e4a4b5f2862d5c
achuasokan <achuasokan555@gmail.com>
2025-09-14 06:40:36 +0530

    added the basic config of express,cors and also db connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+import connectDB from './config/db'
 
+//? dotenv config
+dotenv.config();
+
+//? database connection
+connectDB();
+
+
+//? create express app
 const app = express()
 
 
+//? middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
-export default app
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+}))
+
+
+
+//? exporting the app
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+//? importing the app
+import app from './app'
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
