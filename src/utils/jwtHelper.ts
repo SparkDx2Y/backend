d006814a469d2a96bfec54c994940a5b4a0f70ec
achuasokan <achuasokan555@gmail.com>
2025-12-03 00:33:35 +0530

    added interface for refreshPayload

--- src/utils/jwtHelper.ts
+++ src/utils/jwtHelper.ts
@@ -1,6 +1,11 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { jwtConfig } from "../config/jwtConfig";
 
+interface RefreshPayload {
+  id: string;
+  role: string;
+}
+
 
 export const generateToken = (payload: object): string => {
   return jwt.sign(payload, jwtConfig.accessTokenSecret, {
@@ -19,7 +24,7 @@ export const verifyToken = (token: string) => {
     return jwt.verify(token, jwtConfig.accessTokenSecret)
 }
 
-export const verifyRefreshToken = (token: string) => {
-    return jwt.verify(token, jwtConfig.refreshTokenSecret);
+export const verifyRefreshToken = (token: string): RefreshPayload  => {
+    return jwt.verify(token, jwtConfig.refreshTokenSecret) as RefreshPayload;
   };
   
