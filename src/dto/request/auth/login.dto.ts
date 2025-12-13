cf3cc48274b90ee249291ce93bf060491fa4b1f7
achuasokan <achuasokan555@gmail.com>
2025-12-14 03:15:22 +0530

    fix: chnged the validation logic of register and login scheama and singupSchema in dot

--- src/dto/request/auth/login.dto.ts
+++ src/dto/request/auth/login.dto.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    email: z.email({ message: 'Invalid email address' }).trim(),
-    password: z.string().min(8, { message: 'Password must be at least 8 characters long' }).trim(),
+    email: z.string().email({ message: 'Invalid email address' }).trim(),
+    password: z.string().min(1, { message: 'Password is required' }).trim(),
 })
 
 export type LoginDto = z.infer<typeof loginSchema>;
--- src/dto/request/auth/register.dto.ts
+++ src/dto/request/auth/register.dto.ts
@@ -3,20 +3,20 @@ import { z } from "zod";
 export const signupSchema = z.object({
 
     name: z
-    .string()
-    .min(3, { message: 'Name must be at least 3 characters long' })
-    .max(50, { message: 'Name must be less than 50 characters long' }).trim(),
+        .string()
+        .min(3, { message: 'Name must be at least 3 characters long' })
+        .max(50, { message: 'Name must be less than 50 characters long' }).trim(),
 
-    email: z.email({ message: 'Invalid email address' }).trim(),
+    email: z.string().email({ message: 'Invalid email address' }).trim(),
 
     password: z
-    .string()
-    .min(8, { message: 'Password must be at least 8 characters long' })
-    .max(20, { message: 'Password must be less than 20 characters long' })
-    .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/,
-        { message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character' }
-    ),
+        .string()
+        .min(8, { message: 'Password must be at least 8 characters long' })
+        .max(20, { message: 'Password must be less than 20 characters long' })
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/,
+            { message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character' }
+        ),
 })
 
 export type SignupDto = z.infer<typeof signupSchema>;
