29039d94cc678b5a107037cd4b876a2463cb6ddd
achuasokan <achuasokan555@gmail.com>
2025-12-16 00:42:49 +0530

    fix: changed the min  to length in the validation check of otp length

--- src/dto/request/auth/forgot-password-verify-otp.dto.ts
+++ src/dto/request/auth/forgot-password-verify-otp.dto.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const forgotPasswordVerifyOtpSchema = z.object({
-    otp: z.string().min(6, 'OTP must be at least 6 characters'),
+    otp: z.string().length(6, 'OTP must be at least 6 characters'),
 });
 
 export type ForgotPasswordVerifyOtpDto = z.infer<typeof forgotPasswordVerifyOtpSchema>;
--- src/dto/request/auth/verify-otp.dto.ts
+++ src/dto/request/auth/verify-otp.dto.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const verifyOtpSchema = z.object({
-  otp: z.string().min(6, 'OTP must be at least 6 characters'),
+  otp: z.string().length(6, 'OTP must be at least 6 characters'),
 });
 
 
