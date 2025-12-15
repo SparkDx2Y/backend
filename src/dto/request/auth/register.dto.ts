5bc6da937642cceecd02615409c4220dcb6e16f7
achuasokan <achuasokan555@gmail.com>
2025-12-15 16:01:39 +0530

    fix: changed the max limit of name field to 20 from 50 letters

--- src/dto/request/auth/register.dto.ts
+++ src/dto/request/auth/register.dto.ts
@@ -5,7 +5,7 @@ export const signupSchema = z.object({
     name: z
         .string()
         .min(3, { message: 'Name must be at least 3 characters long' })
-        .max(50, { message: 'Name must be less than 50 characters long' }).trim(),
+        .max(20, { message: 'Name must be less than 20 characters long' }).trim(),
 
     email: z.string().email({ message: 'Invalid email address' }).trim(),
 
