354e4d47829aeefaac51d624e0d01d2b08803973
achuasokan <achuasokan555@gmail.com>
2026-01-07 02:36:38 +0530

    fix:added the zod validation for the body of swipe

--- src/controllers/match/MatchController.ts
+++ src/controllers/match/MatchController.ts
@@ -4,6 +4,7 @@ import { DI_TYPES } from "../../di/types";
 import { IMatchService } from "../../service/match/IMatchService";
 import { HTTP_STATUS } from "../../constants/http-status.constants";
 import { COMMON_ERRORS } from "../../constants/errors/common.erros";
+import { swipeActionSchema } from "../../dto/request/match/swipe-action.dto";
 
 @injectable()
 export class MatchController {
@@ -11,31 +12,33 @@ export class MatchController {
         @inject(DI_TYPES.SERVICES.MATCH_SERVICE) private readonly _matchService: IMatchService
     ) { }
 
-    // GET /match/feed
+   //? Get potential matches for a user (Feed)
     getFeed = async (req: Request, res: Response, next: NextFunction) => {
         try {
+            //? Auth check
             if (!req.user) {
                 return res.status(HTTP_STATUS.UNAUTHORIZED).json({ message: COMMON_ERRORS.UNAUTHORIZED });
             }
-            const profiles = await this._matchService.getPotentialMatches(req.user.id);
+            
+            //? Get potential matches
+            const profiles = await this._matchService.getDiscoverProfiles(req.user.id);
             res.status(HTTP_STATUS.OK).json(profiles);
         } catch (error) {
             next(error);
         }
     };
 
-    // POST /match/action
-    action = async (req: Request, res: Response, next: NextFunction) => {
+    //? Perform a swipe action (Swipe)
+    swipe = async (req: Request, res: Response, next: NextFunction) => {
+        
         try {
+
+            //? Auth check
             if (!req.user) {
                 return res.status(HTTP_STATUS.UNAUTHORIZED).json({ message: COMMON_ERRORS.UNAUTHORIZED });
             }
 
-            const { targetId, action } = req.body;
-
-            if (!targetId || !['like', 'pass'].includes(action)) {
-                return res.status(HTTP_STATUS.BAD_REQUEST).json({ message: "Invalid action payload" });
-            }
+            const { targetId, action } = swipeActionSchema.parse(req.body);
 
             const result = await this._matchService.swipe(req.user.id, targetId, action);
             res.status(HTTP_STATUS.OK).json(result);
