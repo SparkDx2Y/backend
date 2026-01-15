ec87580592dde6335e3ba8a461bf4fe98e9cfbc1
achuasokan <achuasokan555@gmail.com>
2026-01-15 19:57:33 +0530

    feat: include interests in user mapper

--- src/mapper/auth/user.mapper.ts
+++ src/mapper/auth/user.mapper.ts
@@ -3,14 +3,15 @@ import { IUser } from "../../models/user";
 
 
 export class UserMapper {
-    static toUserResponseDto(user: IUser, profilePhoto?: string | null): UserResponseDto {
+    static toUserResponseDto(user: IUser, profilePhoto?: string | null, interests?: string[]): UserResponseDto {
         return {
             id: user._id.toString(),
             name: user.name,
             email: user.email,
             role: user.role,
             isVerified: user.isVerified,
-            profilePhoto: profilePhoto || null
+            profilePhoto: profilePhoto || null,
+            interests: interests || []
         }
     }
 }
