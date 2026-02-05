5136e17875f1f874f6b00603c6b8202524d1ad1b
achuasokan <achuasokan555@gmail.com>
2026-02-06 03:17:17 +0530

    createa file for initilazing  of socketservice and imatchrepisotry or service for instailsie the socketservicewrapper

---
+++ src/socket/index.ts
@@ -0,0 +1,26 @@
+import http from "http";
+import container from "../di";
+import { DI_TYPES } from "../di/types";
+import { SocketServer } from "./SocketServer";
+import { SocketServiceWrapper } from "../service/socket/SocketServiceWrapper";
+import { IMatchedUsersRepository } from "../repositories/match/IMatchedUsersRepository";
+
+
+// ========
+// Initialize Socket.IO
+// ========
+export const initSocket = (httpServer: http.Server) => {
+    const matchedUsersRepo = container.get<IMatchedUsersRepository>(
+        DI_TYPES.REPOSITORIES.MATCHED_USERS_REPOSITORY
+    );
+
+    const socketServer = new SocketServer(httpServer, matchedUsersRepo);
+
+    const socketServiceWrapper = container.get<SocketServiceWrapper>(
+        DI_TYPES.SERVICES.SOCKET_SERVICE
+    );
+
+    socketServiceWrapper.initialize(socketServer);
+
+    return socketServer;
+};
