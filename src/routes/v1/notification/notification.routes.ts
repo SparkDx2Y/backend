c86b277ba658af275a7a91776160d4b2dd3dab39
achuasokan <achuasokan555@gmail.com>
2026-02-05 07:37:00 +0530

    added routes ofr notification

--- src/routes/v1/notification/notification.routes.ts
+++ src/routes/v1/notification/notification.routes.ts
@@ -9,7 +9,6 @@ const router = Router();
 const notificationController = container.get<NotificationController>(DI_TYPES.CONTROLLERS.NOTIFICATION_CONTROLLER);
 
 router.get("/", authMiddleware, notificationController.getNotifications);
-router.get("/unread", authMiddleware, notificationController.getUnreadNotifications);
 router.get("/count", authMiddleware, notificationController.getUnreadCount);
 router.put("/:notificationId/read", authMiddleware, notificationController.markAsRead);
 router.put("/read-all", authMiddleware, notificationController.markAllAsRead);
