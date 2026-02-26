cbc48ac4ded80d0ddd1c5ff33f4da17abf0af111
achuasokan <achuasokan555@gmail.com>
2026-02-26 22:37:31 +0530

    implmented teh service layer of subscription plans creating and updating

---
+++ src/service/subscription/SubscriptionService.ts
@@ -0,0 +1,80 @@
+import { inject, injectable } from "inversify";
+import { ISubscriptionService } from "./ISubscriptionService";
+import { DI_TYPES } from "../../di/types";
+import { ISubscriptionRepository } from "../../repositories/subscription/ISubscriptionRepository";
+import { IUserSubscriptionRepository } from "../../repositories/subscription/IUserSubscriptionRepository";
+import { ISubscriptionPlan } from "../../models/subscription-plan";
+import { IUserSubscription } from "../../models/user-subscription";
+import { SUBSCRIPTION_ERRORS } from "../../constants/errors/subscription.errors";
+import { HTTP_STATUS } from "../../constants/http-status.constants";
+import { AppError } from "../../utils/AppError";
+
+@injectable()
+export class SubscriptionService implements ISubscriptionService {
+    constructor(
+        @inject(DI_TYPES.REPOSITORIES.SUBSCRIPTION_REPOSITORY) private readonly _planRepo: ISubscriptionRepository,
+        @inject(DI_TYPES.REPOSITORIES.USER_SUBSCRIPTION_REPOSITORY) private readonly _userSubRepo: IUserSubscriptionRepository
+    ) { }
+
+    async createPlan(data: Partial<ISubscriptionPlan>): Promise<ISubscriptionPlan> {
+        const existingPlan = await this._planRepo.findByName(data.name!);
+        if (existingPlan) {
+            throw new AppError(SUBSCRIPTION_ERRORS.PLAN_ALREADY_EXISTS, HTTP_STATUS.BAD_REQUEST);
+        }
+
+        return this._planRepo.create(data);
+    }
+
+    async updatePlan(id: string, data: Partial<ISubscriptionPlan>): Promise<ISubscriptionPlan | null> {
+        const existingPlan = await this._planRepo.findById(id);
+        if (!existingPlan) {
+            throw new AppError(SUBSCRIPTION_ERRORS.PLAN_NOT_FOUND, HTTP_STATUS.NOT_FOUND);
+        }
+
+        if (data.name) {
+            const planWithSameName = await this._planRepo.findByName(data.name);
+            if (planWithSameName && planWithSameName._id.toString() !== id) {
+                throw new AppError(SUBSCRIPTION_ERRORS.PLAN_ALREADY_EXISTS, HTTP_STATUS.BAD_REQUEST);
+            }
+        }
+
+        return this._planRepo.updateById(id, data);
+    }
+
+    async getAllPlans(page?: number, limit?: number): Promise<ISubscriptionPlan[] | { plans: ISubscriptionPlan[], pagination: { page: number, limit: number, total: number, totalPages: number } }> {
+        if (page && limit) {
+            const { data, total } = await this._planRepo.findWithPagination(page, limit);
+            return {
+                plans: data,
+                pagination: {
+                    page,
+                    limit,
+                    total,
+                    totalPages: Math.ceil(total / limit)
+                }
+            };
+        }
+        return this._planRepo.find();
+    }
+
+    async getPlanById(id: string): Promise<ISubscriptionPlan | null> {
+        return this._planRepo.findById(id);
+    }
+
+    async togglePlanStatus(id: string): Promise<ISubscriptionPlan | null> {
+        const plan = await this._planRepo.findById(id);
+        if (!plan) {
+            throw new AppError(SUBSCRIPTION_ERRORS.PLAN_NOT_FOUND, HTTP_STATUS.NOT_FOUND);
+        }
+
+        return this._planRepo.updateById(id, { isActive: !plan.isActive });
+    }
+
+    async getActiveSubscription(userId: string): Promise<IUserSubscription | null> {
+        return this._userSubRepo.findActiveByUserId(userId);
+    }
+
+    async getSubscriptionHistory(userId: string): Promise<IUserSubscription[]> {
+        return this._userSubRepo.findAllByUserId(userId);
+    }
+}
